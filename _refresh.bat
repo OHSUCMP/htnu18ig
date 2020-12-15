e4f483a31d044ee3de7df91ee09b389ddcfaf794
ZackAustin <zacksaustin@gmail.com>
2020-12-15 11:38:26 -0700

    Fixing various CQL and IG Structuring bugs for Refresh IG. Refreshed and Bundled IG.

--- _refresh.bat
+++ _refresh.bat
@@ -19,11 +19,11 @@ SET fsoption=-fs http://localhost:8080/cqf-ruler-r4/fhir/
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -cdsig %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -cdsig %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -v -cdsig %fsoption%
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -v -cdsig %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -cdsig %fsoption%
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -cdsig %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -v -cdsig %fsoption%
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -ip="%ig_resource_path%" -rp="%resources_path%" -t -d -p -v -cdsig %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
