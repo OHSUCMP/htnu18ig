1210a7fe2135947b355b880776e53f67a8f3cf46
Amy Yates <yateam@ohsu.edu>
2021-04-09 15:06:01 -0700

    Clean up bad refs for Logica upload and diversify patient info.

--- specification/AddResources.py
+++ specification/AddResources.py
@@ -1,34 +1,38 @@
-# Import CQL resources into the cqf-ruler HAPI FHIR Server
-# Provide the directory where the JSON files live and the url of the FHIR server
-# (This only does one directory at a time, but could easily be adapted to recurse)
+# Import FHIR resources into an open FHIR Server
+# Provide the url of the FHIR server and the base directory where the JSON files live.
+# It will recurse through. By default it loads all resources, but this is problematic with Logica
+# sandbox which checks references. For that load "Patient" resources first.
 import os
 import re
 import requests
 
-directory = "/Users/yateam/HTN/htnu18ig/input/vocabulary/valueset"
-
+directory = "/Users/yateam/HTN/htnu18ig/input/tests/plandefinition/Hypertension"
 print("Loading resources into FHIR server from " + directory)
 override = input("Override? y/n\n")
 if override.lower() == "y":
-	directory = input("The path to the resources\n")
+ 	directory = input("The path to the resources\n")
 
-fhir_url = "http://localhost:8080/cqf-ruler-r4/fhir/"
+fhir_url = "https://api.logicahealth.org/htnu18r42/open/"
 print("FHIR Server URL: " + fhir_url)
 override = input("Override? y/n\n")
 if override.lower() == "y":
-	fhir_url = input("The path to the FHIR server\n")
+    fhir_url = input("The path to the FHIR server\n")
 
-for filename in os.listdir(directory):
-   with open(os.path.join(directory, filename), 'r') as f:
-   		whole = f.read()
-   		p = re.compile('.*"resourceType": "(.*?)",.*', re.MULTILINE | re.DOTALL)
-   		m = p.match(whole)
-   		resourceType = m.group(1)
-   		p = re.compile('.*"id": "(.*?)",.*', re.MULTILINE | re.DOTALL)
-   		m = p.match(whole)
-   		resourceId = m.group(1)
-   		url = fhir_url + resourceType + "/" + resourceId
-   		headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
-   		r = requests.put(url, data=whole, headers=headers)
-   		print(r.text)  		
-   		
+load_resource_type = "all"
+print("Not limiting by Resource Type")
+override = input("Override? y/n\n")
+if (override.lower() == "y"):
+    load_resource_type = input("The resource type to load\n")
+
+for root, dirs, files in os.walk(directory, topdown=False):
+   for name in files:
+       if re.match('.*\.json', name) and not re.match('cqfruler-.*\.json', name):
+           with open(os.path.join(root, name), 'r') as f:
+               whole = f.read()
+               resourceType = re.search('.*"resourceType": "(.*?)",.*', whole).group(1)
+               if (load_resource_type == "all" or load_resource_type.lower() == resourceType.lower()):
+                   resourceId = re.search('.*"id": "(.*?)",.*', whole).group(1)
+                   url = fhir_url + resourceType + "/" + resourceId
+                   headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+                   r = requests.put(url, data=whole, headers=headers)
+                   print(r.text)
