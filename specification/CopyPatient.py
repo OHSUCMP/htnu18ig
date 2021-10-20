718f665bde50b895662e9830a2d8ed424f28329e
Amy Yates <yateam@ohsu.edu>
2021-10-20 15:52:42 -0700

    Resolves #91: Handle SNOMED and ICD-10 OIDs in Condition codes
    This update will transform any FHIR Condition codes from system OID to named system before comparing the code to the approved ValueSet.

--- specification/CopyPatient.py
+++ specification/CopyPatient.py
@@ -2,8 +2,8 @@
 import os
 import re
 
-old_directory = "/Users/yateam/HTN/htnu18ig/input/tests/plandefinition/Pharma/P-AdvancingTreatment"
-new_directory = "/Users/yateam/HTN/htnu18ig/input/tests/plandefinition/Pharma/P-InitialPharmacology"
+old_directory = "/Users/yateam/HTN/htnu18ig/input/tests/plandefinition/Monitoring/M-NotAtGoalAboveGoal"
+new_directory = "/Users/yateam/HTN/htnu18ig/input/tests/plandefinition/Monitoring/M-ConditionBySystemOID"
 
 old_ident = re.search('.*/(.*)$', old_directory).group(1)
 new_ident = re.search('.*/(.*)$', new_directory).group(1)
