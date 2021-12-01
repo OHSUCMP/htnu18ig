414bc787166c7aa5de0e51f8f3c8df45241fa38c
Matthew Storer <storer@ohsu.edu>
2021-12-01 10:45:24 -0800

    tweak

--- _updateCQFTooling.bat
+++ _updateCQFTooling.bat
@@ -6,7 +6,7 @@ ECHO ========
 ECHO Due to FireEye Endpoint Security antivirus scanner (an OHSU thing) identifying 
 ECHO the original batch file as malicious, I've had to modify this script such that 
 ECHO it can still exist and provide some degree of utility.
-ECHO
+ECHO.
 ECHO So now, you must manually download the following file and store it into the
 ECHO input-cache/ folder.  This file can't be stored in the GitHub repo as it's too
 ECHO large, which would have been a good solution but whatever, gotta work around
