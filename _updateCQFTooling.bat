121785c9ea4d99301181d57f63fadcfd5cf73c52
Matthew Storer <storer@ohsu.edu>
2021-12-01 10:44:49 -0800

    tweaks

--- _updateCQFTooling.bat
+++ _updateCQFTooling.bat
@@ -11,8 +11,8 @@ ECHO So now, you must manually download the following file and store it into the
 ECHO input-cache/ folder.  This file can't be stored in the GitHub repo as it's too
 ECHO large, which would have been a good solution but whatever, gotta work around
 ECHO technical limitations.
-ECHO
+ECHO.
 ECHO Dumb.  Whatever.
-ECHO
-ECHO https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.opencds.cqf&a=tooling&v=1.3.1-SNAPSHOT&c=jar-with-dependencies
-ECHO
+ECHO.
+ECHO "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.opencds.cqf&a=tooling&v=1.3.1-SNAPSHOT&c=jar-with-dependencies"
+ECHO.
