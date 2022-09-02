57de38dcd00e68e9e7e6a1e4a0e369b00d020aab
Amy Yates <yateam@ohsu.edu>
2022-09-02 11:40:02 -0700

    Fix future dates and check need for update.

--- specification/UpdateTestDates.py
+++ specification/UpdateTestDates.py
@@ -20,21 +20,24 @@ last_valid_date = datetime.datetime.strptime(date_time, format).date()
 today = datetime.date.today()
 days_to_add = days_between(last_valid_date, today)
 
-directory = os.path.join(root_directory, "input/tests/plandefinition")
+if (days_to_add >= 14):
+    directory = os.path.join(root_directory, "input/tests/plandefinition")
 
-for root, dirs, files in os.walk(directory, topdown=False):
-   for name in files:
-       if re.match('.*\.json', name):
-           with open(os.path.join(root, name), 'r') as f:
-               whole = f.read()
-               dates = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', whole)
-               for date in dates:
-                   the_date = datetime.datetime.strptime(date, format).date()
-                   new_date = the_date + datetime.timedelta(days=days_to_add)
-                   whole = whole.replace(date, new_date.strftime(format))
+    for root, dirs, files in os.walk(directory, topdown=False):
+       for name in files:
+           if re.match('.*\.json', name):
+               with open(os.path.join(root, name), 'r') as f:
+                   whole = f.read()
+                   dates = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', whole)
+                   for date in dates:
+                       the_date = datetime.datetime.strptime(date, format).date()
+                       new_date = the_date + datetime.timedelta(days=days_to_add)
+                       whole = whole.replace(date, new_date.strftime(format))
 
-           with open(os.path.join(root, name), 'w') as f:
-               f.write(whole)
+               with open(os.path.join(root, name), 'w') as f:
+                   f.write(whole)
 
-with open(os.path.join(root_directory, "tests/requests/lastValidDate.txt"), 'w') as f:
-    f.write((last_valid_date + datetime.timedelta(days=days_to_add)).strftime(format))
+    with open(os.path.join(root_directory, "tests/requests/lastValidDate.txt"), 'w') as f:
+        f.write((last_valid_date + datetime.timedelta(days=days_to_add)).strftime(format))
+else:
+    print("This script was run in the last 14 days. Dates do not need updating yet.")
