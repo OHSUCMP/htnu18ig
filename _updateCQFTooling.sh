ed3557e72dd4ba8bbaaad2e6e88c53f1820b4cad
Amy Yates <yateam@ohsu.edu>
2024-02-20 11:00:10 -0800

    Fix updatedCQFTooling script

--- _updateCQFTooling.sh
+++ _updateCQFTooling.sh
@@ -2,18 +2,14 @@
 #DO NOT EDIT WITH WINDOWS
 #exit 1
 
-r=snapshots
-g=org.opencds.cqf
-a=tooling
-v=1.3.1-SNAPSHOT
-c=jar-with-dependencies
+v=1.4.3
 
-dlurl='https://oss.sonatype.org/service/local/artifact/maven/redirect?r='${r}'&g='${g}'&a='${a}'&v='${v}'&c='${c}''
+dlurl='https://oss.sonatype.org/service/local/repo_groups/public/content/org/opencds/cqf/tooling/'${v}'/tooling-'${v}'-jar-with-dependencies.jar'
+tooling_jar='tooling-'${v}'-jar-with-dependencies.jar'
 
 echo ${dlurl}
 
 input_cache_path=./input-cache/
-tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 
 set -e
 if ! type "curl" > /dev/null; then
@@ -21,41 +17,14 @@ if ! type "curl" > /dev/null; then
 	exit 1
 fi
 
-tooling="$input_cache_path$tooling_jar"
-if test -f "$tooling"; then
-	echo "IG Tooling FOUND in input-cache"
-	jarlocation="$tooling"
-	jarlocationname="Input Cache"
-	upgrade=true
-else
-	tooling="../$tooling_jar"
-	upgrade=true
-	if test -f "$tooling"; then
-		echo "IG Tooling FOUND in parent folder"
-		jarlocation="$tooling"
-		jarlocationname="Parent Folder"
-		upgrade=true
-	else
-		echo IG Tooling NOT FOUND in input-cache or parent folder...
-		jarlocation="$input_cache_path$tooling_jar"
-		jarlocationname="Input Cache"
-		upgrade=false
-	fi
-fi
-
-if $upgrade ; then
-	message="Overwrite $jarlocation? [Y/N] "
-else
-	#echo Will place tooling jar here: $input_cache_path$tooling_jar
-	echo Will place tooling jar here: $jarlocation
-	message="Ok? [Y/N]"
-fi
+jarlocation="$input_cache_path$tooling_jar"
+echo Will place tooling jar here: $jarlocation
+message="Ok? [Y/N]"
 
 read -r -p "$message" response
 if [[ "$response" =~ ^([yY])$ ]]; then
-	echo "Downloading most recent tooling to $jarlocationname - it's ~170 MB, so this may take a bit"
-#	wget "https://oss.sonatype.org/service/local/repositories/snapshots/content/org/opencds/cqf/tooling/1.0-SNAPSHOT/tooling-1.0-20200107.163002-6-jar-with-dependencies.jar" -O "$jarlocation"
-	curl $dlurl -L -o "$jarlocation" --create-dirs
+	wget -O "${jarlocation}" "${dlurl}"
+	#curl $dlurl -L -o "$jarlocation" --create-dirs
 	echo "Download complete."
 else
 	echo cancel...
